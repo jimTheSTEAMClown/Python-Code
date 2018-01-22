1a99526b50f3fcc7e09a6709876ede7e01688b9a
Jim Burnham <jim.the.steam.clown@gmail.com>
2018-01-21 16:23:18 -0800

    Create aTemplate2.py

---
+++ aTemplate2.py
@@ -0,0 +1,38 @@
+# ============================================================================
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/Python-Code
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 3.0.
+# https://creativecommons.org/licenses/by-nc-sa/3.0/
+# Program/Design Name:		aTemplate.py
+# Description:    This is a template for python programs
+# Dependencies:   python3
+# Revision: 
+#  Revision 0.02 - Updated 01/21/2018 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 01/20/2018
+# Additional Comments: 
+# 
+# ============================================================================
+
+# this is myFunction, which is called from main
+def myFunction():
+  print("just entered myFunction"
+  z=z+(x-y)
+  print(z)
+  print("Just ran myFunction")
+
+def main():
+  # This is the main function.  all your main code goes here.  
+  # You can call other functions from here
+  x=2
+  y=6
+  z=x+y
+  print(z)
+  print("calling myFunction")
+  myFunction()
+  print('all done with main')
+  # All done
+
+# This is the call to the Function main().  You should always 
+# have a main() and def main(): as part of all your programs
+main()
