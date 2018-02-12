dc564b357954add8b224b9d8afe09d2fdb6c2ef8
Jim Burnham <jim.the.steam.clown@gmail.com>
2018-02-11 21:17:01 -0800

    Create function-py4e.py

---
+++ function-py4e.py
@@ -0,0 +1,29 @@
+# ============================================================================
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/Python-Code
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 3.0.
+# https://creativecommons.org/licenses/by-nc-sa/3.0/
+# Program/Design Name:		function-py4e.py
+# Description:    This is an example of a function from https://www.py4e.com/lessons/functions
+# Dependencies:   python3
+# Revision: 
+#  Revision 0.02 - Updated 02/11/2018 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 02/11/2018
+# Additional Comments: 
+# 
+# ============================================================================
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+def main():
+    x = 5
+    print('Hello')
+    print('Yo')
+    x = x + 2
+    print(x)
+
+# This is the call to the Function main().  You should always 
+# have a main() and def main(): as part of all your programs
+main()
