cfd47c50ee147a2140a6ca7273c72a2b025b6778
Jim Burnham <jim.the.steam.clown@gmail.com>
2018-02-11 21:54:54 -0800

    Update lang-py4e.py

--- lang-py4e.py
+++ lang-py4e.py
@@ -4,12 +4,13 @@
 # Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
 # This example code is licensed under the CC BY-NC-SA 3.0.
 # https://creativecommons.org/licenses/by-nc-sa/3.0/
-# Program/Design Name:		aTemplate.py
-# Description:    This is a template for python programs
+# Program/Design Name:		lang-py4e.py
+# Description:    This is a template for passing parameters to a function
+# See https://www.py4e.com/lessons/functions
 # Dependencies:   python3
 # Revision: 
-#  Revision 0.02 - Updated 01/21/2018 for SVCTE Mechatronics Class
-#  Revision 0.01 - Created 01/20/2018
+#  Revision 0.02 - Updated 02/11/2018 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 02/11/2018
 # Additional Comments: 
 # 
 # ============================================================================
